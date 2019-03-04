67217f8f9d16fd05c2d36d72971c91e266eca140
Shane Clancy <clancys@oregonstate.edu>
2019-03-04 14:21:00 -0800

    Updated my tests for foxy

--- test/FoxyTest.java
+++ test/FoxyTest.java
@@ -1,5 +1,16 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Assert;
+import org.junit.Test;
 
 class FoxyTest {
-
+    @Test
+    public void tickTest(){
+        Window.cameraLocation = "Pirate Cove";
+        Office.monitorUp = true;
+        f.tick();
+        Office.door1open = true;
+        Office.monitorUp = false;
+        for(int i = 0; i < 2000; i++){
+            f.tick();
+        }
+    }
 }
