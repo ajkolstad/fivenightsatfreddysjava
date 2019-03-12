7864f9cb222a09cf103bd36becfcc16352637263
Carter Olsen <olsencar@oregonstate.edu>
2019-03-12 13:30:20 -0700

    modified:   .idea/workspace.xml 	deleted:    bin/BonnieInitializeTest.class 	deleted:    bin/BonnieTest.class 	deleted:    bin/ChickaInitializeTest.class 	deleted:    bin/ChickaTest.class 	modified:   bin/FoxyInitializeTest.class 	modified:   bin/FoxyTest.class 	deleted:    bin/FreddyInitializeTest.class 	deleted:    bin/FreddyTest.class 	deleted:    bin/MainTest.class 	modified:   test/FoxyInitializeTest.java 	modified:   test/FoxyTest.java

--- test/FoxyInitializeTest.java
+++ test/FoxyInitializeTest.java
@@ -1,5 +1,5 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
 
-class FoxyInitializeTest {
+public class FoxyInitializeTest {
 
 }
--- test/FoxyTest.java
+++ test/FoxyTest.java
@@ -1,5 +1,5 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
 
-class FoxyTest {
+public class FoxyTest {
 
 }
